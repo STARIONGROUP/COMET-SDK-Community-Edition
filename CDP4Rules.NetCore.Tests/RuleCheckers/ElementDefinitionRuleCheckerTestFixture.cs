df31e7ee4e0a940d8de6edd15dacfbfaaca7b098
samatrhea <s.gerene@rheagroup.com>
2019-09-01 13:44:07 +0200

    [Add] ElementDefinitionRuleChecker for MA-0610 and MA-0620

---
+++ CDP4Rules.NetCore.Tests/RuleCheckers/ElementDefinitionRuleCheckerTestFixture.cs
@@ -0,0 +1,136 @@
+﻿// <copyright file="ElementDefinitionRuleCheckerTestFixture.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2019 RHEA System S.A.
+//
+//    Author: Sam Gerené
+//
+//    This file is part of CDP4-SDK Community Edition
+//
+//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Rules.NetCore.Tests.RuleCheckers
+{
+    using System;
+    using System.Linq;
+    using CDP4Common.CommonData;
+    using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Rules.Common;
+    using CDP4Rules.RuleCheckers;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="ElementDefinitionRuleChecker"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ElementDefinitionRuleCheckerTestFixture
+    {
+        private ElementDefinitionRuleChecker elementDefinitionRuleChecker;
+
+        private ElementDefinition elementDefinition;
+
+        private Parameter Parameter_1;
+        
+        private Parameter Parameter_2;
+
+        private TextParameterType textParameterType_1;
+
+        private TextParameterType textParameterType_2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.elementDefinitionRuleChecker = new ElementDefinitionRuleChecker();
+
+            this.elementDefinition = new ElementDefinition();
+            this.Parameter_1 = new Parameter { Iid = Guid.Parse("d9ac20be-1578-47cc-81e1-a60c8533c266") };
+            this.Parameter_2 = new Parameter { Iid = Guid.Parse("7b67663b-13c6-40c2-b31a-28b109333ee3") };
+            this.textParameterType_1 = new TextParameterType();
+            this.textParameterType_2 = new TextParameterType();
+
+            this.elementDefinition.Parameter.Add(this.Parameter_1);
+            this.elementDefinition.Parameter.Add(this.Parameter_2);
+
+            this.Parameter_1.ParameterType = textParameterType_1;
+            this.Parameter_2.ParameterType = textParameterType_2;
+        }
+
+        [Test]
+        public void Verify_that_when_Check_is_called_with_null_exception_isThrown()
+        {
+            Assert.Throws<ArgumentNullException>(() => this.elementDefinitionRuleChecker.CheckWhetherContainerParametersHaveUniqueShortNames(null));
+        }
+
+        [Test]
+        public void Verify_that_when_Check_is_called_with_not_ElementDefinition_exception_isThrown()
+        {
+            var alias = new Alias();
+            Assert.Throws<ArgumentException>(() => this.elementDefinitionRuleChecker.CheckWhetherContainerParametersHaveUniqueShortNames(alias));
+        }
+
+        [Test]
+        public void Verify_that_when_ElementDefinition_contains_parameters_with_duplicate_shortnames_a_result_is_returned()
+        {
+            this.textParameterType_1.ShortName = "TEXT";
+            this.textParameterType_2.ShortName = "TEXT";
+
+            var result = this.elementDefinitionRuleChecker.CheckWhetherContainerParametersHaveUniqueShortNames(this.elementDefinition).Single();
+
+            Assert.That(result.Id, Is.EqualTo("MA-0620"));
+            Assert.That(result.Description, Is.EqualTo("The ElementDefinition contains Parameters with non-unique short names: d9ac20be-1578-47cc-81e1-a60c8533c266,7b67663b-13c6-40c2-b31a-28b109333ee3:TEXT,TEXT"));
+            Assert.That(result.Severity, Is.EqualTo(SeverityKind.Error));
+            Assert.That(result.Thing, Is.EqualTo(this.elementDefinition));
+        }
+
+        [Test]
+        public void Verify_that_when_ElementDefinition_contains_parameters_with_unique_shortnames_no_result_is_returned()
+        {
+            this.textParameterType_1.ShortName = "TEXT_1";
+            this.textParameterType_2.ShortName = "TEXT_2";
+
+            var results = this.elementDefinitionRuleChecker.CheckWhetherContainerParametersHaveUniqueShortNames(this.elementDefinition);
+
+            Assert.That(results, Is.Empty);
+        }
+
+        [Test]
+        public void Verify_that_when_ElementDefinition_contains_parameters_with_duplicate_names_a_result_is_returned()
+        {
+            this.textParameterType_1.Name = "TEXT";
+            this.textParameterType_2.Name = "TEXT";
+
+            var result = this.elementDefinitionRuleChecker.CheckWhetherContainerParametersHaveUniqueNames(this.elementDefinition).Single();
+
+            Assert.That(result.Id, Is.EqualTo("MA-0610"));
+            Assert.That(result.Description, Is.EqualTo("The ElementDefinition contains Parameters with non-unique names: d9ac20be-1578-47cc-81e1-a60c8533c266,7b67663b-13c6-40c2-b31a-28b109333ee3:TEXT,TEXT"));
+            Assert.That(result.Severity, Is.EqualTo(SeverityKind.Error));
+            Assert.That(result.Thing, Is.EqualTo(this.elementDefinition));
+        }
+
+        [Test]
+        public void Verify_that_when_ElementDefinition_contains_parameters_with_unique_names_no_result_is_returned()
+        {
+            this.textParameterType_1.Name = "TEXT_1";
+            this.textParameterType_2.Name = "TEXT_2";
+
+            var results = this.elementDefinitionRuleChecker.CheckWhetherContainerParametersHaveUniqueNames(this.elementDefinition);
+
+            Assert.That(results, Is.Empty);
+        }
+    }
+}
+
+
---
+++ CDP4Rules/RuleCheckers/ElementDefinitionRuleChecker.cs
@@ -0,0 +1,147 @@
+﻿// <copyright file="ElementDefinitionRuleChecker.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2019 RHEA System S.A.
+//
+//    Author: Sam Gerené
+//
+//    This file is part of CDP4-SDK Community Edition
+//
+//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Rules.RuleCheckers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using CDP4Common.CommonData;
+    using CDP4Common.EngineeringModelData;
+    using CDP4Rules.Common;
+
+    /// <summary>
+    /// The purpose of the <see cref="ElementDefinitionRuleChecker"/> is to execute the rules for instances of type <see cref="ElementDefinition"/>
+    /// </summary>
+    [RuleChecker(typeof(ElementDefinition))]
+    public class ElementDefinitionRuleChecker : RuleChecker
+    {
+        /// <summary>
+        /// Checks whether the contained <see cref="Parameter"/>s have unique shortnames
+        /// </summary>
+        /// <param name="thing">
+        /// The subject <see cref="ElementDefinition"/>
+        /// </param>
+        /// <returns>
+        /// An <see cref="IEnumerable{RuleCheckResult}"/> which is empty when no rule violations are encountered.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// thrown when <paramref name="thing"/> is null
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// thrown when <paramref name="thing"/> is not an <see cref="ElementDefinition"/>
+        /// </exception>
+        [Rule("MA-0610")]
+        public IEnumerable<RuleCheckResult> CheckWhetherContainerParametersHaveUniqueNames(Thing thing)
+        {
+            var elementDefinition = this.VerifyThingArgument(thing);
+
+            var results = new List<RuleCheckResult>();
+            var ruleAttribute = System.Reflection.MethodBase.GetCurrentMethod().GetCustomAttribute<RuleAttribute>();
+            var rule = StaticRuleProvider.QueryRules().Single(r => r.Id == ruleAttribute.Id);
+
+            var duplicates = elementDefinition.Parameter.GroupBy(p => p.ParameterType.Name).Where(grp => grp.Count() > 1).SelectMany(d => d).ToList();
+
+            if (duplicates.Any())
+            {
+                var duplicateIdentifiers = string.Join(",", duplicates.Select(p => p.Iid));
+                var duplicateNames = string.Join(",", duplicates.Select(p => p.ParameterType.Name));
+
+                var result = new RuleCheckResult(thing, rule.Id, $"The ElementDefinition contains Parameters with non-unique names: {duplicateIdentifiers}:{duplicateNames}", SeverityKind.Error);
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Checks whether the contained <see cref="Parameter"/>s have unique shortnames
+        /// </summary>
+        /// <param name="thing">
+        /// The subject <see cref="ElementDefinition"/>
+        /// </param>
+        /// <returns>
+        /// An <see cref="IEnumerable{RuleCheckResult}"/> which is empty when no rule violations are encountered.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// thrown when <paramref name="thing"/> is null
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// thrown when <paramref name="thing"/> is not an <see cref="ElementDefinition"/>
+        /// </exception>
+        [Rule("MA-0620")]
+        public IEnumerable<RuleCheckResult> CheckWhetherContainerParametersHaveUniqueShortNames(Thing thing)
+        {
+            var elementDefinition = this.VerifyThingArgument(thing);
+
+            var results = new List<RuleCheckResult>();
+            var ruleAttribute = System.Reflection.MethodBase.GetCurrentMethod().GetCustomAttribute<RuleAttribute>();
+            var rule = StaticRuleProvider.QueryRules().Single(r => r.Id == ruleAttribute.Id);
+
+            var duplicates = elementDefinition.Parameter.GroupBy(p => p.ParameterType.ShortName).Where(grp => grp.Count() > 1).SelectMany(d => d).ToList();
+
+            if (duplicates.Any())
+            {
+                var duplicateIdentifiers = string.Join(",", duplicates.Select(p => p.Iid));
+                var duplicateShortNames = string.Join(",", duplicates.Select(p => p.ParameterType.ShortName));
+
+                var result = new RuleCheckResult(thing, rule.Id, $"The ElementDefinition contains Parameters with non-unique short names: {duplicateIdentifiers}:{duplicateShortNames}", SeverityKind.Error);
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Verifies that the <see cref="Thing"/> is of the correct type
+        /// </summary>
+        /// <param name="thing">
+        /// the subject <see cref="Thing"/>
+        /// </param>
+        /// <returns>
+        /// an instance of <see cref="ElementDefinition"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// thrown when <paramref name="thing"/> is null
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// thrown when <paramref name="thing"/> is not an <see cref="ElementDefinition"/>
+        /// </exception>
+        private ElementDefinition VerifyThingArgument(Thing thing)
+        {
+            if (thing == null)
+            {
+                throw new ArgumentNullException($"The {nameof(thing)} may not be null");
+            }
+
+            var elementDefinition = thing as ElementDefinition;
+            if (elementDefinition == null)
+            {
+                throw new ArgumentException($"{nameof(thing)} with Iid:{thing.Iid} is not an ElementDefinition");
+            }
+
+            return elementDefinition;
+        }
+    }
+}
