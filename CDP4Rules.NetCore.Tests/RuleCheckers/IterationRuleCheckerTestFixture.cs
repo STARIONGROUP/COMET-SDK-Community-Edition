cf888f68de89df466963b52dbdafcf70fccb2fd5
samatrhea <s.gerene@rheagroup.com>
2019-10-29 20:00:41 +0100

    fixes #72
    Summary: [Add] implementation of MA-0900
    
    Test Plan: run unit tests
    
    Reviewers: alexander.vandelft, avorobiev
    
    Reviewed By: alexander.vandelft, avorobiev
    
    Subscribers: naron.phou
    
    Differential Revision: http://phabricator.rheagroup.com/D1521

---
+++ CDP4Rules.NetCore.Tests/RuleCheckers/IterationRuleCheckerTestFixture.cs
@@ -0,0 +1,118 @@
+﻿// <copyright file="IterationRuleChecker.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2019 RHEA System S.A.
+//
+//    Author: Sam Gerené
+//
+//    This file is part of CDP4-SDK Community Edition
+//
+//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Rules.NetCore.Tests.RuleCheckers
+{
+    using System;
+    using System.Linq;
+    using CDP4Rules.Common;
+    using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Rules.RuleCheckers;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="IterationRuleChecker"/> class.
+    /// </summary>
+    [TestFixture]
+    public class IterationRuleCheckerTestFixture
+    {
+        private IterationRuleChecker iterationRuleChecker;
+
+        private EngineeringModelSetup engineeringModelSetup;
+
+        private IterationSetup iterationSetup;
+
+        private EngineeringModel engineeringModel;
+
+        private Iteration iteration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.iterationRuleChecker = new IterationRuleChecker();
+
+            this.engineeringModelSetup = new EngineeringModelSetup();
+            this.iterationSetup = new IterationSetup();
+            this.engineeringModelSetup.IterationSetup.Add(iterationSetup);
+
+            this.engineeringModel = new EngineeringModel();
+            this.iteration = new Iteration { Iid = Guid.Parse("37076675-f0cd-47e8-8ab4-1d2e7025f591") };
+            this.engineeringModel.Iteration.Add(iteration);
+
+            this.engineeringModel.EngineeringModelSetup = this.engineeringModelSetup;
+            this.iteration.IterationSetup = this.iterationSetup;
+        }
+
+        [Test]
+        public void Verify_that_when_thing_is_not_an_Iteration_an_exception_is_thrown()
+        {
+            var elementDefinition = new ElementDefinition();
+
+            Assert.Throws<ArgumentException>(() => this.iterationRuleChecker.CheckWhetherACatalogueContainsNoMoreThanOneOptionPerIteration(elementDefinition));
+        }
+
+        [Test]
+        public void Verify_that_when_Iteration_is_null_exception_is_thrown()
+        {
+            this.iteration = null;
+            Assert.Throws<ArgumentNullException>(() => this.iterationRuleChecker.CheckWhetherACatalogueContainsNoMoreThanOneOptionPerIteration(iteration));
+        }
+
+        [Test]
+        public void Verify_that_when_catalogue_contains_more_than_one_option_result_is_returned()
+        {
+            this.engineeringModelSetup.Kind = EngineeringModelKind.MODEL_CATALOGUE;
+            
+            var option_1 = new Option();
+            var option_2 = new Option();
+
+            this.iteration.Option.Add(option_1);
+            this.iteration.Option.Add(option_2);
+
+            var results = this.iterationRuleChecker.CheckWhetherACatalogueContainsNoMoreThanOneOptionPerIteration(this.iteration);
+            var result = results.Single();
+
+            Assert.That(result.Id, Is.EqualTo("MA-0900"));
+            Assert.That(result.Severity, Is.EqualTo(SeverityKind.Warning));
+            Assert.That(result.Description, Is.EqualTo("The Iteration 37076675-f0cd-47e8-8ab4-1d2e7025f591 is contained by a MODEL_CATALOGUE and contains more than one Option"));
+            Assert.That(result.Thing, Is.EqualTo(this.iteration));
+        }
+
+        [Test]
+        public void Verify_that_when_an_EngineeringModel_is_not_a_catalogue_and_contains_more_than_one_option_no_result_is_returned()
+        {
+            this.engineeringModelSetup.Kind = EngineeringModelKind.STUDY_MODEL;
+
+            var option_1 = new Option();
+            var option_2 = new Option();
+
+            this.iteration.Option.Add(option_1);
+            this.iteration.Option.Add(option_2);
+
+            var results = this.iterationRuleChecker.CheckWhetherACatalogueContainsNoMoreThanOneOptionPerIteration(this.iteration);
+            
+            Assert.That(results, Is.Empty);
+        }
+    }
+}
---
+++ CDP4Rules/RuleCheckers/IterationRuleChecker.cs
@@ -0,0 +1,107 @@
+﻿// <copyright file="IterationRuleChecker.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2019 RHEA System S.A.
+//
+//    Author: Sam Gerené
+//
+//    This file is part of CDP4-SDK Community Edition
+//
+//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Rules.RuleCheckers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using CDP4Common.CommonData;
+    using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Rules.Common;
+
+    /// <summary>
+    /// The purpose of the <see cref="IterationRuleChecker"/> is to execute the rules for instances of type <see cref="Iteration"/>
+    /// </summary>
+    [RuleChecker(typeof(Iteration))]
+    public class IterationRuleChecker : RuleChecker
+    {
+        /// <summary>
+        /// Checks whether the <see cref="Iteration"/> contains no more than one <see cref="Option"/>
+        /// </summary>
+        /// <param name="thing">
+        /// The subject <see cref="Iteration"/>
+        /// </param>
+        /// <returns>
+        /// An <see cref="IEnumerable{RuleCheckResult}"/> which is empty when no rule violations are encountered.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// thrown when <paramref name="thing"/> is null
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// thrown when <paramref name="thing"/> is not an <see cref="Iteration"/>
+        /// </exception>
+        [Rule("MA-0900")]
+        public IEnumerable<RuleCheckResult> CheckWhetherACatalogueContainsNoMoreThanOneOptionPerIteration(Thing thing)
+        {
+            var iteration = this.VerifyThingArgument(thing);
+
+            var results = new List<RuleCheckResult>();
+            var ruleAttribute = System.Reflection.MethodBase.GetCurrentMethod().GetCustomAttribute<RuleAttribute>();
+            var rule = StaticRuleProvider.QueryRules().Single(r => r.Id == ruleAttribute.Id);
+
+            var engineeringModel = (EngineeringModel) iteration.Container;
+            if (engineeringModel.EngineeringModelSetup.Kind == EngineeringModelKind.MODEL_CATALOGUE &&
+                iteration.Option.Count > 1)
+            {
+                var result = new RuleCheckResult(thing, rule.Id, $"The Iteration {iteration.Iid} is contained by a MODEL_CATALOGUE and contains more than one Option", SeverityKind.Warning);
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Verifies that the <see cref="Thing"/> is of the correct type
+        /// </summary>
+        /// <param name="thing">
+        /// the subject <see cref="Thing"/>
+        /// </param>
+        /// <returns>
+        /// an instance of <see cref="ElementDefinition"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// thrown when <paramref name="thing"/> is null
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// thrown when <paramref name="thing"/> is not an <see cref="ElementDefinition"/>
+        /// </exception>
+        private Iteration VerifyThingArgument(Thing thing)
+        {
+            if (thing == null)
+            {
+                throw new ArgumentNullException($"The {nameof(thing)} may not be null");
+            }
+
+            var iteration = thing as Iteration;
+            if (iteration == null)
+            {
+                throw new ArgumentException($"{nameof(thing)} with Iid:{thing.Iid} is not an Iteration");
+            }
+
+            return iteration;
+        }
+    }
+}
