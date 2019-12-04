cd6a1bba999add230fa1d3b26b5863185bf152d6
Alexander van Delft <a.vandelft@rheagroup.com>
2019-12-04 11:12:21 +0100

    Renaming and commenting after review

--- CDP4Common/Poco/RelationalOperatorKindMethods.cs
+++ CDP4Common/Poco/RelationalOperatorKindMethods.cs
@@ -1,4 +1,27 @@
-﻿namespace CDP4Common.EngineeringModelData
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RelationalOperatorKindMethods.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2019 RHEA System S.A.
+//
+//    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou, Alexander van Delft, Yevhen Ikonnykov
+//
+//    This file is part of CDP4-SDK Community Edition
+//
+//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CDP4Common.EngineeringModelData
 {
     /// <summary>
     /// Extension methods for the <see cref="RelationalOperatorKind"/> enum
@@ -9,8 +32,8 @@
         /// Extension method that converts a <see cref="RelationalOperatorKind"/> to a readable string
         /// </summary>
         /// <param name="value"><see cref="RelationalOperatorKind"/> to convert</param>
-        /// <returns></returns>
-        public static string ToFriendlyString(this RelationalOperatorKind value)
+        /// <returns>String that contains a scientific representation of the <see cref="RelationalOperatorKind"/></returns>
+        public static string ToScientificNotationString(this RelationalOperatorKind value)
         {
             switch (value)
             {
