27237e2e63cbb4e263195ba6f8a6c39ed955d6c8
samatrhea <s.gerene@rheagroup.com>
2020-07-03 16:37:47 +0200

    [Remove] unused using statement; fixes https://sonarcloud.io/project/issues?id=RHEAGROUP_CDP4-SDK-Community-Edition&issues=AXMUfUqf4PCXjoHPXXlz&open=AXMUfUqf4PCXjoHPXXlz

--- CDP4Common.Tests/Poco/ParametricConstraintTestFixture.cs
+++ CDP4Common.Tests/Poco/ParametricConstraintTestFixture.cs
@@ -1,7 +1,6 @@
-﻿#region Copyright
-// --------------------------------------------------------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ParametricConstraintTestFixture.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2019 RHEA System S.A.
+//    Copyright (c) 2015-2020 RHEA System S.A.
 //
 //    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou
 //
@@ -22,15 +21,15 @@
 //    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-#endregion
 
 namespace CDP4Common.Tests.Poco
 {
     using System;
+
     using CDP4Common.EngineeringModelData;
     using CDP4Common.Exceptions;
     using CDP4Common.SiteDirectoryData;
-    using Exceptions;
+    
     using NUnit.Framework;
 
     [TestFixture]
