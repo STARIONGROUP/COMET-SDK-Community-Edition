3a14cf63e0f2df9a5d562906c552f332f3b5f063
a.vorobiev <a.vorobiev@rheagroup.com>
2021-01-11 20:16:43 +0100

    Bump versions to 1.2.0 for accepted extensions.

--- CDP4ServicesDal/Headers.cs
+++ CDP4ServicesDal/Headers.cs
@@ -67,6 +67,6 @@ namespace CDP4ServicesDal
         /// <summary>
         /// The header that specifies the version of CDP extensions that are accepted
         /// </summary>
-        internal const string AcceptCdpVersionValue = "1.1.0";
+        internal const string AcceptCdpVersionValue = "1.2.0";
     }
 }
