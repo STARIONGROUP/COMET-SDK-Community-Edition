6c4258572ea40edf3da0cedc17ebf23709952884
samatrhea <s.gerene@rheagroup.com>
2021-03-23 10:48:15 +0100

    [remove] preprocessor statement where no longer required

--- CDP4JsonSerializer/Helper/Utils.cs
+++ CDP4JsonSerializer/Helper/Utils.cs
@@ -1,17 +1,17 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------------------------------------------
 // <copyright file="Utils.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2019 RHEA System S.A.
+//    Copyright (c) 2015-2021 RHEA System S.A.
 //
-//    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou
+//    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou, Alexandervan Delft, Nathanael Smiechowski, Ahmed Abulwafa Ahmed
 //
-//    This file is part of CDP4-SDK Community Edition
+//    This file is part of COMET-SDK Community Edition
 //
-//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    The COMET-SDK Community Edition is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU Lesser General Public
 //    License as published by the Free Software Foundation; either
 //    version 3 of the License, or (at your option) any later version.
 //
-//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    The COMET-SDK Community Edition is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //    Lesser General Public License for more details.
@@ -20,8 +20,7 @@
 //    along with this program; if not, write to the Free Software Foundation,
 //    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
+// -------------------------------------------------------------------------------------------------------------------------------
 namespace CDP4JsonSerializer.Helper
 {
     using System;
@@ -52,12 +51,7 @@ namespace CDP4JsonSerializer.Helper
                 throw new ArgumentException("string can't be empty!");
             }
 
-#if NET45 || NET451 || NET452 || NET46 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
             return $"{input.First().ToString(CultureInfo.InvariantCulture).ToUpper()}{input.Substring(1)}";
-#else
-            return $"{input.First().ToString().ToUpper()}{input.Substring(1)}";
-#endif
-
         }
 
         /// <summary>
@@ -78,11 +72,8 @@ namespace CDP4JsonSerializer.Helper
             {
                 throw new ArgumentException("string can't be empty!");
             }
-#if NET45 || NET451 || NET452 || NET46 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
+
             return $"{input.First().ToString(CultureInfo.InvariantCulture).ToLower()}{input.Substring(1)}";
-#else
-            return $"{input.First().ToString().ToLower()}{input.Substring(1)}";
-#endif
         }
     }
 }
