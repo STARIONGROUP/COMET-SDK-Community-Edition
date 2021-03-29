d60c0bf33463d5af6d714d4b22a87a3b8d96675b
samatrhea <s.gerene@rheagroup.com>
2021-03-29 13:56:41 +0200

    [Add] ThingNotFoundException; refactor JsonFileDalUtils to check null on SiteDirectoryDto and throw ThingNotFoundException; fixes https://sonarcloud.io/project/issues?id=RHEAGROUP_CDP4-SDK-Community-Edition&issues=AXMUfUzN4PCXjoHPXXnY&open=AXMUfUzN4PCXjoHPXXnY

--- CDP4JsonFileDal/JsonFileDalUtils.cs
+++ CDP4JsonFileDal/JsonFileDalUtils.cs
@@ -1,17 +1,17 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="JsonFileDalUtils.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2019 RHEA System S.A.
+//    Copyright (c) 2015-2021 RHEA System S.A.
 //
 //    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou
 //
-//    This file is part of CDP4-SDK Community Edition
+//    This file is part of COMET-SDK Community Edition
 //
-//    The CDP4-SDK Community Edition is free software; you can redistribute it and/or
+//    The COMET-SDK Community Edition is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU Lesser General Public
 //    License as published by the Free Software Foundation; either
 //    version 3 of the License, or (at your option) any later version.
 //
-//    The CDP4-SDK Community Edition is distributed in the hope that it will be useful,
+//    The COMET-SDK Community Edition is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //    Lesser General Public License for more details.
@@ -29,6 +29,7 @@ namespace CDP4JsonFileDal
 
     using CDP4Common.CommonData;
     using CDP4Common.EngineeringModelData;
+    using CDP4Common.Exceptions;
     using CDP4Common.SiteDirectoryData;
 
     using CDP4JsonFileDal.Json;
@@ -303,6 +304,11 @@ namespace CDP4JsonFileDal
                         dtos.Add(siteDirectoryItem.ToDto());
                     }
                 }
+            }
+
+            if (siteDirectoryDto == null)
+            {
+                throw new ThingNotFoundException("The SiteDirectory DTO could not be found");
             }
 
             // remove the domains-of-expertise that should not be included in the cloned SiteDirectory
