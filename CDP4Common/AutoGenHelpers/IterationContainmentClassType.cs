f7dd260de03fac7ccd05256fd0999153fce7adec
Omar Elebiary <o.elebiary@rheagroup.com>
2022-08-04 09:11:49 +0200

    Implement possible relationship ClassKind list; fixes #237

---
+++ CDP4Common/AutoGenHelpers/IterationContainmentClassType.cs
@@ -0,0 +1,104 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IterationContainmentClassType.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2022 RHEA System S.A.
+//
+//    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou, Alexander van Delft, Nathanael Smiechowski, Omar Elebiary
+//
+//    This file is part of COMET-SDK Community Edition
+//    This is an auto-generated class. Any manual changes to this file will be overwritten!
+//
+//    The COMET-SDK Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The COMET-SDK Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Common.Helpers
+{
+    using CDP4Common.CommonData;
+    using CDP4Common.DTO;
+
+    /// <summary>
+    /// Class representing the Array of <see cref="ClassKind"/>'s contained by <see cref= "Iteration"/>
+    /// </summary>
+    public static class IterationContainmentClassType
+    {
+        /// <summary>
+        /// Array consisting of <see cref="ClassKind"/>'s which are contained through a composite aggregation by <see cref= "EngineeringModel"/> and 
+        /// it's subtree, excluding the subtree of the <see cref="Iteration"/> class
+        /// </summary>
+        /// <remarks>
+        /// The array does not contain any ClassKinds of abstract classes
+        /// </remarks>
+        public static readonly ClassKind[] ClassKindArray = new[] {
+            ClassKind.ActualFiniteState,
+            ClassKind.ActualFiniteStateList,
+            ClassKind.Alias,
+            ClassKind.AndExpression,
+            ClassKind.BinaryRelationship,
+            ClassKind.Bounds,
+            ClassKind.BuiltInRuleVerification,
+            ClassKind.Citation,
+            ClassKind.Color,
+            ClassKind.Definition,
+            ClassKind.DiagramCanvas,
+            ClassKind.DiagramEdge,
+            ClassKind.DiagramObject,
+            ClassKind.DomainFileStore,
+            ClassKind.ElementDefinition,
+            ClassKind.ElementUsage,
+            ClassKind.ExclusiveOrExpression,
+            ClassKind.ExternalIdentifierMap,
+            ClassKind.File,
+            ClassKind.FileRevision,
+            ClassKind.Folder,
+            ClassKind.Goal,
+            ClassKind.HyperLink,
+            ClassKind.IdCorrespondence,
+            ClassKind.MultiRelationship,
+            ClassKind.NestedElement,
+            ClassKind.NestedParameter,
+            ClassKind.NotExpression,
+            ClassKind.Option,
+            ClassKind.OrExpression,
+            ClassKind.OwnedStyle,
+            ClassKind.Parameter,
+            ClassKind.ParameterGroup,
+            ClassKind.ParameterOverride,
+            ClassKind.ParameterOverrideValueSet,
+            ClassKind.ParameterSubscription,
+            ClassKind.ParameterSubscriptionValueSet,
+            ClassKind.ParameterValueSet,
+            ClassKind.ParametricConstraint,
+            ClassKind.Point,
+            ClassKind.PossibleFiniteState,
+            ClassKind.PossibleFiniteStateList,
+            ClassKind.Publication,
+            ClassKind.RelationalExpression,
+            ClassKind.RelationshipParameterValue,
+            ClassKind.Requirement,
+            ClassKind.RequirementsContainerParameterValue,
+            ClassKind.RequirementsGroup,
+            ClassKind.RequirementsSpecification,
+            ClassKind.RuleVerificationList,
+            ClassKind.RuleViolation,
+            ClassKind.SharedStyle,
+            ClassKind.SimpleParameterValue,
+            ClassKind.Stakeholder,
+            ClassKind.StakeholderValue,
+            ClassKind.StakeHolderValueMap,
+            ClassKind.StakeHolderValueMapSettings,
+            ClassKind.UserRuleVerification,
+            ClassKind.ValueGroup,
+        };
+    }
+}
